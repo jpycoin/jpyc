5cdd3e987fb7b3b85818a654b079d1e2f7ed004d
hayatoVTA <hayato.xxx.8810@gmail.com>
2021-10-11 21:28:50 +0900

    fix: lang setting

--- src/i18n.js
+++ src/i18n.js
@@ -1,59 +1,65 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import translationEN from './locales/en.json';
-import translationJA from './locales/ja.json';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+// import translationEN from "./locales/en.json";
+import translationJA from "./locales/ja.json";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const dataI18n = document.querySelectorAll("[data-i18n]");
 
-  const dataI18n = document.querySelectorAll('[data-i18n]');
-  
   const detectorOptions = {
-    order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag'],
-    lookupQuerystring: 'lng',
-    lookupCookie: 'i18next',
-    lookupLocalStorage: 'i18nextLng',
-    lookupSessionStorage: 'i18nextLng',
-    caches: ['localStorage'],
-    excludeCacheFor: ['cimode', 'cookie'],
+    order: [
+      "querystring",
+      "cookie",
+      "localStorage",
+      "sessionStorage",
+      "navigator",
+      "htmlTag",
+    ],
+    lookupQuerystring: "lng",
+    lookupCookie: "i18next",
+    lookupLocalStorage: "i18nextLng",
+    lookupSessionStorage: "i18nextLng",
+    caches: ["localStorage"],
+    excludeCacheFor: ["cimode", "cookie"],
     cookieMinutes: 10,
-    cookieDomain: 'https://jpyc.jp',
+    cookieDomain: "https://jpyc.jp",
   };
-  
+
   i18n
     .use(LanguageDetector)
     .use(initReactI18next)
     .init(
-        {
-            fallbackLng: () => {
-              const lang = window.navigator.language;
-              if (lang === 'ja') {
-                return ['ja'];
-              } else {
-                return ['en'];
-              }
-            },
-            resources: {
-              en: {
-                translation: translationEN
-              },
-              ja: {
-                translation: translationJA
-              }
-            },
-            detection: detectorOptions,
-            react: {
-              wait: true,
-            }
+      {
+        fallbackLng: () => {
+          const lang = window.navigator.language;
+          // if (lang === 'ja') {
+          //   return ['ja'];
+          // } else {
+          //   return ['en'];
+          // }
+          return ["ja"];
         },
-        (e, t) => {
-            if (e) return console.log('something went wrong loading', e);
-            dataI18n.forEach((elem) => {
-                elem.textContent = t(elem.dataset.i18n);
-            });
-        }
+        resources: {
+          // en: {
+          //   translation: translationEN,
+          // },
+          ja: {
+            translation: translationJA,
+          },
+        },
+        detection: detectorOptions,
+        react: {
+          wait: true,
+        },
+      },
+      (e, t) => {
+        if (e) return console.log("something went wrong loading", e);
+        dataI18n.forEach((elem) => {
+          elem.textContent = t(elem.dataset.i18n);
+        });
+      }
     );
-
 });
 
 export default i18n;
